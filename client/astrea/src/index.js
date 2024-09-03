42ecde250df71220fcb90689a16bcd272e899bfc
MoonDog11 <123662244+MoonDog11@users.noreply.github.com>
2024-09-03 14:59:20 -0500

    Update index.js

--- client/astrea/src/index.js
+++ client/astrea/src/index.js
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux'; // Ensure createStore is imported from 'redux'
 import rootReducer from './Redux/Reducer'; // Ensure the path is correct
 import App from './App';
-
 import reportWebVitals from './reportWebVitals';
 
 // Create the Redux store
@@ -13,10 +12,11 @@ const store = createStore(rootReducer);
 // Create the root element and render the app
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
-    <App />
-
-  </Provider>
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
 
 // Report web vitals (optional)
