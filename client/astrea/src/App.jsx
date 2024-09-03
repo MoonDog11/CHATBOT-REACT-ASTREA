7a0106e6472c7a3bb5a8770162942919e7e3f855
MoonDog11 <123662244+MoonDog11@users.noreply.github.com>
2024-09-03 15:44:59 -0500

    Update App.jsx

--- client/astrea/src/App.jsx
+++ client/astrea/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter,Routes, Route } from 'react-router-dom';
-import ChatBot from './Comopents/ChatBot'; 
+import ChatBot from './Components/ChatBot';
+
 
 import { Provider } from 'react-redux';
 import store from "./Redux/Store";
